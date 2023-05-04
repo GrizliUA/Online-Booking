0c2639bbe4b835c13c43aa8f5491313b86772593
Oleksandr Rad <clavigo365@gmail.com>
2023-05-04 17:26:14 +0300

    Added Jinja2

--- online-booking-app/app.py
+++ online-booking-app/app.py
@@ -2,8 +2,21 @@
 from flask import Flask, render_template, request, redirect, make_response
 
 
-
 app = Flask(__name__)
+
+def response_400(response='Bad Request'):
+    """Returns response with code 400"""
+    return make_response(response,400)
+
+
+@app.route('/' , methods=['GET'])
+def main():
+    """main page function"""
+    try:
+        return render_template('child.html')
+    except:
+        return response_400()
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
