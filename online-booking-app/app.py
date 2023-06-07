9d8027751490cf03605ad62f625f613ccfa49a50
Maksym Struk <maksum07770@gmail.com>
2023-06-07 14:24:04 +0300

    Up to Date

--- online-booking-app/app.py
+++ online-booking-app/app.py
@@ -1,7 +1,7 @@
 """Modules providing Flask realising hosting web-application at local instance"""
 import json
 from flask_mysqldb import MySQL
-from flask import Flask, render_template, request, redirect, make_response
+from flask import Flask, render_template, request, redirect, make_response, jsonify
 from flask_restful import Resource, Api
 from models.models import *
 from database_details import Database_dt
@@ -34,13 +34,13 @@ def response_404(response='Not Found'):
     """Returns response with code 404"""
     return make_response(response,404)
 
-@app.route('/error' , methods=['GET'])
-def error():
-    """Error page function"""
-    try:
-        return render_template('error.html')
-    except:
-        return response_400()
+# @app.route('/' , methods=['GET'])
+# def error():
+#     """Error page function"""
+#     try:
+#         return render_template('child.html')
+#     except:
+#         return response_400()
 
 def category_response(categories_data):
     return {"category_id": categories_data[0][0], "category_label": categories_data[0][1]}
@@ -55,82 +55,101 @@ def item_response(item_data):
             "item_date": item_data[0][6],
             "item_score": item_data[0][7]}
 
-def obj_to_list(obj) -> list:
+# def obj_to_list(obj) -> list:
+#     new_list = []
+#     for row in obj:
+#         keys = row.__table__.columns.keys()
+#         values = []
+#         for col in keys:
+#             values.append(getattr(row, col))
+
+
+#         #new_list.append(list((getattr(row, col)) for col in row.__table__.columns.keys()))
+#     return new_list
+
+
+def obj_to_json(obj) -> str:
     new_list = []
     for row in obj:
-        new_list.append(list((getattr(row, col)) for col in row.__table__.columns.keys()))
-    return new_list
+        row_dict = {col: getattr(row, col) for col in row.__table__.columns.keys()}
+        new_list.append(row_dict)
+    return json.dumps(new_list)
+
+
+
+
+
 
 @app.route('/' , methods=['GET'])
 def main():
     """Main page function"""
     try:
-        categories_data = obj_to_list(Categories.query.all())
-        return render_template('main.html', categories=categories_data)
+        categories_data = obj_to_json(City.query.all())
+        return response_200(categories_data)
     except:
         return redirect("http://127.0.0.1:5000/error")
 
 
-class Api(Resource):
-    def get(self):
-        #try:
-        flight = Flight()
-        response = {"uid": flight.flight_ID,
-                    "fli_id": flight.flights_ID,
-                    "p_id": flight.passanger_ID,
-                    "price": flight.price,}
-        return response_200(response)
-        #except:
-            #return response_400()
-api.add_resource(Api, '/api')
+# class Api(Resource):
+#     def get(self):
+#         #try:
+#         flight = Flight()
+#         response = {"uid": flight.flight_ID,
+#                     "fli_id": flight.flights_ID,
+#                     "p_id": flight.passanger_ID,
+#                     "price": flight.price,}
+#         return response_200(response)
+#         #except:
+#             #return response_400()
+# api.add_resource(Api, '/api')
 
-class ApiItemSearch(Resource):
-    """Item API Search realization"""
-    def get(self):
-        """API Item Search"""
-        try:
-            data = ''
+# class ApiItemSearch(Resource):
+#     """Item API Search realization"""
+#     def get(self):
+#         """API Item Search"""
+#         try:
+#             data = ''
 
-            for key, value in request.args.items():
-                data += f'{key} = "{value}", '
-            data = data[:-2]
+#             for key, value in request.args.items():
+#                 data += f'{key} = "{value}", '
+#             data = data[:-2]
 
-            if data == '':
-                return response_400()
+#             if data == '':
+#                 return response_400()
             
 
-            item_data = obj_to_list(Items.query.filter_by(**eval('dict(' + data + ')')).all())
+#             item_data = obj_to_list(Items.query.filter_by(**eval('dict(' + data + ')')).all())
 
-            if not item_data:
-                return response_404()
+#             if not item_data:
+#                 return response_404()
 
-            response = item_response(item_data)
+#             response = item_response(item_data)
 
-            return response_200(response)
-        except:
-            return response_400()
-api.add_resource(ApiItemSearch, '/api/item/search/')
+#             return response_200(response)
+#         except:
+#             return response_400()
+# api.add_resource(ApiItemSearch, '/api/item/search/')
 
-@app.route('/item/searching', methods=['GET', 'POST'])
-def item_searching():
-    """Search page function"""
-    try:
-        if len(request.form["item_label"]) > 0 and len(request.form["item_date"]) > 0:
-            url_req = urlencode({"item_label": request.form["item_label"], "item_date": request.form["item_date"]})
-        elif len(request.form["item_label"]) > 0:
-            url_req = urlencode({"item_label": request.form["item_label"]})
-        elif len(request.form["item_date"]) > 0:
-            url_req = urlencode({"item_date": request.form["item_date"]})
-        url = "http://127.0.0.1:5000/api/item/search/?" + url_req
+# @app.route('/item/searching', methods=['GET', 'POST'])
+# def item_searching():
+#     """Search page function"""
+#     try:
+#         if len(request.form["item_label"]) > 0 and len(request.form["item_date"]) > 0:
+#             url_req = urlencode({"item_label": request.form["item_label"], "item_date": request.form["item_date"]})
+#         elif len(request.form["item_label"]) > 0:
+#             url_req = urlencode({"item_label": request.form["item_label"]})
+#         elif len(request.form["item_date"]) > 0:
+#             url_req = urlencode({"item_date": request.form["item_date"]})
+#         url = "http://127.0.0.1:5000/api/item/search/?" + url_req
 
-        with urlopen(url) as req:
-            res = req.read()
-        dict_res = json.loads(res)
-        values_list = list(dict_res.values())
+#         with urlopen(url) as req:
+#             res = req.read()
+#         dict_res = json.loads(res)
+#         values_list = list(dict_res.values())
 
-        return redirect(f"http://127.0.0.1:5000/item/{values_list[2]}")
-    except:
-        return redirect("http://127.0.0.1:5000/item/search")
+#         return redirect(f"http://127.0.0.1:5000/item/{values_list[2]}")
+#     except:
+#         return redirect("http://127.0.0.1:5000/item/search")
 
 
 
@@ -437,20 +456,6 @@ def item_searching():
 #             return response_400()
 # api.add_resource(ApiItemSearch, '/api/item/search/')
 
-
-
-def response_400(response='Bad Request'):
-    """Returns response with code 400"""
-    return make_response(response,400)
-
-
-@app.route('/' , methods=['GET'])
-def main():
-    """main page function"""
-    try:
-        return render_template('child.html')
-    except:
-        return response_400()
 
 
 if __name__ == "__main__":
